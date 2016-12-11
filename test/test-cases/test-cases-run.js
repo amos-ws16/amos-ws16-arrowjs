4d4f25ad720ef35f2afde72920648dea252017f3
Kevin Marcel Styp-Rekowski <k.styp-rekowski@campus.tu-berlin.de>
2016-12-11 19:06:55 +0100

    TestCase Auswertung hinzugefuegt, abfragen geschehen nun als POST-Requests und von dort wird der total-score entnommen, unabhängig davon, welche plugins versagen oder welche parameter fehlen

--- test/test-cases/test-cases-run.js
+++ test/test-cases/test-cases-run.js
@@ -1,47 +1,56 @@
-
-// const fs = require('fs')
-const scoreManager = require('../../lib/score-manager')
-const config = require('../../config/index.js')
-const manager = scoreManager.create(config.scoreManager)
+const buster = require('buster')
+const request = require('supertest')
+const app = require('../../lib/server')
 
 let allTestCases = {}
-// other testcases don't work yet, some errors occur because some fields are missing
-// allTestCases.testCasesDuc = require('./duc-test-cases.js').testCases
-// allTestCases.testCasesFabian = require('./fabian-test-cases').testCases
-// allTestCases.testCasesJan = require('./jan-test-cases').testCases
+// allTestCases get loaded
+allTestCases.testCasesDuc = require('./duc-test-cases.js').testCases
+allTestCases.testCasesFabian = require('./fabian-test-cases').testCases
+allTestCases.testCasesJan = require('./jan-test-cases').testCases
 allTestCases.testCasesSimon = require('./simon-test-cases').testCases
-// allTestCases.testCasesYves = require('./yves-test-cases').testCases
+allTestCases.testCasesYves = require('./yves-test-cases').testCases
 
 // throws error cause testCases are not valid
 // allTestCases = getAllTestCases()
 
+var hitCounter = 0
+var length = 0
 for (let key in allTestCases) {
   var testCases = allTestCases[key]
-  var hitCounter = 0
+  length += testCases.length
   console.log('TestCase name: ' + key)
   for (let index in testCases) {
     let testCase = testCases[index]
-    const a = manager.score(testCase)
-    let biggestAIndex = 0
-    let biggestAScore = 0
-    for (let j in a) {
-      if (a[j].total > biggestAScore) {
-        biggestAIndex = j
-        biggestAScore = a[j].total
-      }
-    }
-    console.log('groessterIndex:' + biggestAIndex + ', groesster Score:' + biggestAScore)
-    if (biggestAIndex === '0') {
-      hitCounter++
-    }
-  }
-  const hitRate = hitCounter / testCases.length
+    request(app)
+      .post('/api/score')
+      .send(testCase)
+      .end(function (err, res) {
+        buster.refute(err)
 
-  console.log('Länge: ' + testCases.length)
+        let biggestAIndex = 0
+        let biggestAScore = 0
+        for (let j in res.body.result) {
+          if (res.body.result[j].total > biggestAScore) {
+            biggestAIndex = j
+            biggestAScore = res.body.result[j].total
+          }
+        }
+        if (biggestAIndex === '0') {
+          hitCounter++
+        }
+        console.log('groessterIndex:' + biggestAIndex + ', groesster Score:' + biggestAScore)
+      })
+  }
+}
+// Timeout needed because supertest runs tests asynchronously and nesting callbacks for 60 tests wouldn't be feasible.
+// No better workaround found than waiting 3 seconds until hopefully all asynchronous tests have run through.
+setTimeout(function () {
+  const hitRate = hitCounter / length
+  console.log('Länge: ' + length)
   console.log('Hits: ' + hitCounter)
   console.log((hitRate * 100) + '% wurden korrekt bewertet.')
   console.log('______________________________________')
-}
+}, 3000)
 
 /**
 *dynamic loads of all testcases in the folder ('./test/test-cases') with the format of <NAME>-test-cases.js
