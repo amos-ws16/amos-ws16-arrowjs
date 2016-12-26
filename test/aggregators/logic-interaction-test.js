3059b8b79570c288e4a1a7f6f91007c891526974
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2016-12-26 20:56:30 +0100

    Add test for logic aggregators' interaction.

---
+++ test/aggregators/logic-interaction-test.js
@@ -0,0 +1,28 @@
+const buster = require('buster')
+
+const or = require('../../lib/aggregators/or')
+const and = require('../../lib/aggregators/and')
+const not = require('../../lib/aggregators/not')
+const nand = require('../../lib/aggregators/nand')
+
+buster.testCase('logic aggregators', {
+  'should be able to represent or as and expression': function () {
+    let zero = { eval: this.stub().returns(0.0) }
+    let one = { eval: this.stub().returns(1.0) }
+
+    let A = or.create([ zero, one ])
+    let B = not.create(nand.create([ zero, one ]))
+
+    buster.assert.near(A.eval(), B.eval(), 1e-3)
+  },
+
+  'should be able to represent and as or expression': function () {
+    let zero = { eval: this.stub().returns(0.0) }
+    let one = { eval: this.stub().returns(1.0) }
+
+    let A = and.create([ zero, one ])
+    let B = not.create(or.create([ not.create(zero), not.create(one) ]))
+
+    buster.assert.near(A.eval(), B.eval(), 1e-3)
+  }
+})
