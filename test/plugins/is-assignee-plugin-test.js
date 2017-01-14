d6cd5d49f54177c0a019d02799d6503a45f6e69a
dn0912 <nguyen.duc0912@googlemail.com>
2017-01-14 16:54:16 +0100

    testing cases

--- test/plugins/is-assignee-plugin-test.js
+++ test/plugins/is-assignee-plugin-test.js
@@ -2,7 +2,11 @@ const buster = require('buster')
 const plugin = require('../../lib/plugins/is-assignee-plugin.js')
 
 buster.testCase('isAssigneePlugin', {
-  'should return ': function () {
+  'should return 1.0 if  fileuploader is an assignee of a task': function () {
+    buster.assert.same(plugin())
+  },
+
+  'should return 0.0 if  fileuploader is not an assignee of a task': function () {
     buster.assert.same(plugin())
   }
 })
