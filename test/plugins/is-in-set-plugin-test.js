996693d942ae13e60bb00ed6c01a6db06d0c974b
Duc Nguyen <duc.nguyen@kyto.com>
2017-01-23 13:44:46 +0100

    exception handling

--- test/plugins/is-in-set-plugin-test.js
+++ test/plugins/is-in-set-plugin-test.js
@@ -21,13 +21,27 @@ buster.testCase('isInSetPlugin', {
   },
 
   'Testcase 4: no user and no assignees': function () {
-    let user = ''
+    let user = 'a'
     let assignees = []
     buster.assert.equals(plugin(user, assignees), 0.0)
   },
   'Testcase 5: no user provided': function () {
-    let user = ''
+    let user = 'b'
     let assignees = ['a', 'c']
     buster.assert.equals(plugin(user, assignees), 0.0)
+  },
+  'Testcase 6 - should throw an error if first input is undefined': function () {
+    let assignees = 111
+    buster.assert.exception(() => plugin(assignees))
+  },
+  'Testcase 7 - should throw an error if second input is not an array': function () {
+    let user = 'a'
+    let assignees = 111
+    buster.assert.exception(() => plugin(user, assignees))
+  },
+  'Testcase 8 - should throw an error if first input is not type of array input type': function () {
+    let user = 111
+    let assignees = ['a', 'c']
+    buster.assert.exception(() => plugin(user, assignees))
   }
 })
