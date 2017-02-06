ebca375912c0a59c2a22130c35276bab47e2d788
Kevin Marcel Styp-Rekowski <k.styp-rekowski@campus.tu-berlin.de>
2017-02-06 19:44:52 +0100

    renamed day-of-week plugin to clearly show that the day-name is returned

--- test/pipes/day-name-of-week-test.js
+++ test/pipes/day-name-of-week-test.js
@@ -1,7 +1,7 @@
 const buster = require('buster')
-const pipe = require('../../lib/pipes/day-of-week')
+const pipe = require('../../lib/pipes/day-name-of-week')
 
-buster.testCase('Pipe: dayOfWeek', {
+buster.testCase('Pipe: dayNameOfWeek', {
   'should return day of week': function () {
     var testTime = new Date('January 05, 2017 9:00:00 UTC')
     let input = testTime.getTime() / 1000
